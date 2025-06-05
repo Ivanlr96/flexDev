3b4aebbe6f9d450e6650ecb0fc835b66bf5a91c7
Iván Lorenzo <elhibar77@hotmail.es>
2025-06-05 12:52:44 +0200

    Añadido el atributo al botón ver más de las cards y modificada la función para cargar la información

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const modal = document.getElementById('profile-modal');
   const modalContent = document.getElementById('modal-content');
   const closeBtn = document.getElementById('close-modal');
-  const btnSee = document.querySelector('.btn-see');
+  const btnSeeList = document.querySelectorAll('.btn-see');
   const blur = document.getElementById('blur');
-  
 
-  btnSee.addEventListener('click', function(e) {
-    e.preventDefault();
-    fetch('plantilla/ivan.html')
-      .then(res => res.text())
-      .then(html => {
-        modalContent.innerHTML = html;
-        modal.showModal();
-        blur.classList.add('blur-bg');
-  
-      });
+  btnSeeList.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      const profile = btn.getAttribute('data-profile');
+      fetch('plantilla/' + profile)
+        .then(res => res.text())
+        .then(html => {
+          modalContent.innerHTML = html;
+          modal.showModal();
+          blur.classList.add('blur-bg');
+        });
+    });
   });
 
   closeBtn.addEventListener('click', function() {
